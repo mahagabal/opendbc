58b1e43920db7f3278a6068c0b814a5c971770aa
Your Name <you@example.com>
2025-03-18 10:00:03 +0100

    pushed

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-#DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+#DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null &&pwd)"
 #cd $DIR
 python opendbc/car/vin.py
 #
